180e53405cba1b6ef7f4548a2d80f3d034215266
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-03-15 18:33:56 +0100

    First group commit We "implemented" card and slavers

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Card.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Card.java
@@ -1,6 +1,20 @@
 package it.polimi.ingsw.psp23;
+
+import java.util.List;
+
 public abstract class Card {
-    void Play(){
-        System.out.println("Playing Card");
+    private int level;
+
+    Card(int level) {
+        this.level = level;
     }
+
+    //we used an abstract method so that every subclass has to implement his own method separately
+    public abstract void play(List<Player> players);
+
+    // we are using a getter because we want to know the level of the card later(fro example when we have to create the small decks)
+    public int getLevel() {
+        return level;
+    }
+
 }
---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Slavers.java
@@ -0,0 +1,47 @@
+package it.polimi.ingsw.psp23;
+
+import java.util.List;
+
+public class Slavers extends Card{
+    private int cannonStrength;
+    private int membersStolen;
+    private int prize;
+    private int days;
+
+    Slavers(int level, int cannonStrength, int membersStolen, int prize, int days){
+        super(level);
+        this.cannonStrength = cannonStrength;
+        this.membersStolen = membersStolen;
+        this.prize = prize;
+        this.days = days;
+    }
+
+    public void play(List<Player> players){
+        int i = 0;
+        Boolean defeated = false;
+        int option;
+
+        while(!defeated && i < players.size()){
+            // do you want to use any batteries?
+
+            if(players.get(i).getTruck().getFirePower() < cannonStrength){
+                // qui dobbiamo gestire il fatto che il player perde
+            }
+            else if(players.get(i).getTruck().getFirePower() > cannonStrength){
+                defeated = true;
+                // opzione di scelta tra il prendere i crediti cosmici ma perdere
+                // i giorni oppure non perdere giorni ma non prendere i crediti cosmici
+                option = players.get(i).decide();
+                if(option == 0){
+                    players.get(i).perdigiorni
+                }
+                else if(option == 1){
+
+                }
+            }
+
+            i++;
+        }
+    }
+
+}
