2a72d901f6cf343bf035fa5aabb2fd49c3391b62
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-03-17 21:24:48 +0100

    removed abstract from Card

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Card.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Card.java
@@ -2,15 +2,17 @@ package it.polimi.ingsw.psp23;
 
 import java.util.List;
 
-public abstract class Card {
+public class Card {
     private int level;
 
     Card(int level) {
         this.level = level;
     }
 
-    //we used an abstract method so that every subclass has to implement his own method separately
-    public abstract void play(List<Player> players);
+
+    public void play(List<Player> players){
+
+    };
 
     // we are using a getter because we want to know the level of the card later(fro example when we have to create the small decks)
     public int getLevel() {
