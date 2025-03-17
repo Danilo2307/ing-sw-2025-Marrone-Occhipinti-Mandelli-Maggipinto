8792fb5b8c1605126e78646cf312c242d5e90a0e
Danilo2307 <danilo.marrone@mail.polimi.it>
2025-03-17 22:43:03 +0100

    Container class and Item class

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Container.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.psp23;
+import java.util.List;
+
+public class Container extends Component {
+    private int size;
+    private Color colorContainer;
+    private List<Item> goods;
+
+    Container(int size, Color colorContainer, List<Item> goods, Side up, Side down, Side left, Side right, int x, int y) {
+        super(up,down,left,right,x,y);
+        this.size = size;
+        this.colorContainer = colorContainer;
+        this.goods = goods;
+    }
+
+    public List<Item> getItems(){
+        return  goods;
+    }
+
+    public void loadItem(Item item){
+        if(goods.size() < size ) {
+            if(colorContainer == Color.Red && (item.getItemColor() == Color.Red || item.getItemColor() == Color.Green) ||
+            colorContainer == Color.Blue && !(item.getItemColor() == Color.Red))
+                goods.add(item);
+            else
+                System.out.println("You cannot add this item here");
+        }else{
+            System.out.println("The size of goods is greater than the size of size " + size);
+        }
+    }
+
+    public void loseItem(Item item){
+        goods.remove(item); //qui la remove effettua un confronto tra gli attributi di item che in realtà è solo color
+                            // e ne rimuove la prima occorrenza
+    }
+}
+
