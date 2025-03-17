c3572b478a15b644ec4cf85d17befe4f50bca328
Danilo2307 <danilo.marrone@mail.polimi.it>
2025-03-17 16:42:28 +0100

    Finished CombatZone with an addition class CannonShot to handle the CombatZone card

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/CannonShot.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.psp23;
+
+public class CannonShot {
+    private final boolean big;
+    private final Direction direction;
+
+    CannonShot(boolean big, Direction direction) {
+        this.big = big;
+        this.direction = direction;
+    }
+
+    public boolean isBig() {
+        return big;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+}
--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/CombatZone.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/CombatZone.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.psp23;
 
-public class CombatZone {
+public class CombatZone extends Card {
     // Danilo
 
     //legenda dei char: E: engine strength
@@ -19,14 +19,16 @@ public class CombatZone {
     int daysLost;
     int goodsLost;
     int membersLost;
+    private final List<CannonShot> cannonshot;
 
-    public CombatZone(int daysLost, int goodsLost, int membersLost,String penalty1,String penalty2, String penalty3) {
+    public CombatZone(int daysLost, int goodsLost, int membersLost,String penalty1,String penalty2, String penalty3,List<CannonShot> cannonshot) {
         this.daysLost = daysLost;
         this.goodsLost = goodsLost;
         this.membersLost = membersLost;
         this.penalty1 = penalty1;
         this.penalty2 = penalty2;
         this.penalty3 = penalty3;
+        this.cannonshot = cannonshot;
     }
 
     public String getPenalty1(int i) {
@@ -41,9 +43,12 @@ public class CombatZone {
     }
 
     public void play(List<Player> players){
+        CannonShot c;
+        int impactLine;
         int i, pos;
         int size = players.size();
         int tmp;
+        //inizio prima sfida
         if(penalty1.equals("Members")){
             for(i=0;i<size;i++){
                 if(i==0) {
@@ -56,19 +61,82 @@ public class CombatZone {
             }
             System.out.println(players.get(i).nickname+" has less crew members!");
             players.get(i).updatePosition(-daysLost);
-        }else if(penalty2.equals("Cannon_strength")){
+        }else if(penalty1.equals("Cannon_strength")){
             for(i=0;i<size;i++){
                 if(i==0) {
                     tmp = players.get(i).getTruck().getCannonStrength();
                     pos = i;
                 }else if(tmp > players.get(i).getTruck().getCannonStrength()){
-                    tmp = players.get(i).getTruck().getCannonStrength();
+                    tmp = players.get(i).getTruck().getCannonStrength(); //get cannon strength si occuperà dell'input sulle batterie
                     pos = i;
                 }
             }
+            System.out.println(players.get(pos).nickname+" has less cannon strength!");
+            players.get(pos).updatePosition(-daysLost);
+
+
         }
+
+//inizio seconda sfida
+            for(i=0;i<size;i++){
+                if(i==0) {
+                    tmp = players.get(i).getTruck().getEngineStrength();
+                    pos = i;
+                }else if(tmp > players.get(i).getTruck().getEngineStrength()){
+                    tmp = players.get(i).getTruck().getEngineStrength(); //get engine strength si occuperà dell'input sulle batterie
+                    pos = i;
+                }
+            }
+
+        System.out.println(players.get(pos).nickname + " has less engine strength!");
+
+        if(penalty2.equals("Members")) {
+            players.get(pos).getTruck().reduceHubCrew(membersLost);
+            players.get(pos).getTruck().reduceCrew(membersLost);
+        } else if(penalty2.equals("Goods")){
+            players.get(pos).getTruck().pickMostImportantGoods(goodsLost);
+        }
+
+        //inizio terza sfida
+        if(penalty3.equals("Members")){
+            for(i=0;i<size;i++){
+                if(i==0) {
+                    tmp = players.get(i).getTruck().crew;
+                    pos = i;
+                }else if(tmp > players.get(i).getTruck().crew) {
+                    tmp = players.get(i).getTruck().crew;
+                    pos = i;
+                }
+            }
+
+            System.out.println(players.get(pos).nickname + " has less crew members!");
+            for(c: cannonshot){
+                impactLine = DiceUtility.roll2to12();
+                players.get(pos).getTruck().handleCannon(c,impactLine);
+            }
+
+
+
+        }else if(penalty3.equals("Cannon_strength")){
+            for(i=0;i<size;i++){
+                if(i==0) {
+                    tmp = players.get(i).getTruck().getCannonStrength();
+                    pos = i;
+                }else if(tmp > players.get(i).getTruck().getCannonStrength()){
+                    tmp = players.get(i).getTruck().getCannonStrength(); //get cannon strength si occuperà dell'input sulle batterie
+                    pos = i;
+                }
+            }
+            System.out.println(players.get(pos).nickname + " has less cannon strength!");
+            for(c: cannonshot){
+                impactLine = DiceUtility.roll2to12();
+                players.get(pos).getTruck().handleCannon(c,impactLine);
+            }
+        }
+
+
     }
 
 
 
-    }
+}
