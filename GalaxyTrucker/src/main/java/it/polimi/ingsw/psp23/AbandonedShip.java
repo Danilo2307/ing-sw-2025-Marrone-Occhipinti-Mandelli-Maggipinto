faf70ac0411c8104f65c32a85300d689e24a33b0
Danilo2307 <danilo.marrone@mail.polimi.it>
2025-03-17 20:05:21 +0100

    Finished Pirates

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/AbandonedShip.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/AbandonedShip.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.psp23;
 
-public class AbandonedShip {
+public class AbandonedShip extends Card{
     //Danilo
 
     int days;
@@ -29,11 +29,11 @@ public class AbandonedShip {
         boolean isSold = false;
         int size = Players.size();
         int i = 0;
-        while(i < size){
+        while(i < size && !isSold){
             if(Players.get(i).getTruck().crew > numMembers && Players.get(i).isInGame) {
                 isSold = true; // qui ricevo la risposta del player e la metto nella variabile isSold
                 if (isSold) {
-                    Players.get(i).getTruck().reduceHubCrew(numMembers); //serve a scegliere da quali hub togliere i membri
+                   //in reducehubcrew verrà anche deciso da quali hub togliere i membri da eliminare
                     Players.get(i).getTruck().crew.reduceCrew(numMembers);
                     Players.get(i).updateMoney(cosmicCredits);
                     Players.get(i).updatePosition(-days);
