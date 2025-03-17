ac72ed6dc2bb9e27b3896e461e047f2c7680d982
gigimaggi03 <luigi.maggipinto@mail.polimi.it>
2025-03-17 20:45:14 +0100

    Created Cards: Stardust & AbandonedStation

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Startdust.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Startdust.java
@@ -1,5 +1,20 @@
 package it.polimi.ingsw.psp23;
 
-public class Startdust {
-    // Luigi
+import java.util.List;
+
+public class Stardust extends Card {
+
+    public Stardust(int level) {
+        super(level);
+    }
+
+    @Override
+    public void play(List<Player> players) {
+        for (Player player : players) {
+            if (player.isInGame()) {
+                int penalty = player.getTruck().getExposedConnectors();
+                player.updatePosition(-penalty);
+            }
+        }
+    }
 }
