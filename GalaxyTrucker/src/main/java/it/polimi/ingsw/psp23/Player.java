5e56e66885caee66e6b9e9eb16d9a8d5aa728184
Federico Mandelli <10827992@polimi.it>
2025-03-18 11:52:01 +0100

    Player "principale"". mancano molti metodi su cui ho dubbi. ne parliamo dopo

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Player.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Player.java
@@ -1,4 +1,41 @@
 package it.polimi.ingsw.psp23;
 
 public class Player {
+    private final String nickname;
+    private int position ;
+    private Board truck;
+    private int money;
+    private boolean inGame;
+
+    public Player(String nickname) {
+        this.nickname = nickname;
+        this.position = 0;
+        this.truck = new Board();
+        this.money = 0;
+        this.inGame = true;
+    }
+
+    public boolean isInGame() {
+        return inGame;
+    }
+
+    public Board getTruck() {
+        return new Board(truck);
+    }
+
+    public void updateMoney(int moneyVariation) {
+        money += moneyVariation;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void leaveGame() {
+        inGame = false;
+    }
+
+
+
+
 }
