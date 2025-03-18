a9d390be530b340082813dc907332036ee58765e
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-03-18 17:35:12 +0100

    aggiornamento 2

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/OpenSpace.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/OpenSpace.java
@@ -14,8 +14,7 @@ public class OpenSpace extends Card{
     // sapendo che quando usiamo il getter della potenza motrice del player ci arriva già il valore aggiornato della potenza motrice, in cui è considerato
     // l'eventuale uso di batterie per i motori doppi, basta che open space faccia avanzare le posizioni delle pedine.
     // Per l'avanzamento devo assicurarmi di avanzare effettivamente di un numero di POSIZIONI VUOTE pari al numero della potenza motrice, sorpassando
-    // eventualmente i giocatori davanti a me e non contando la casella dove c'è l'altro giocatore, per questo mi basta fare check sulla posizione del giocatore e,
-    // mano a mano che avanzo, controllo che la mia posizione non sia uguale a quella di qualsiasi altro player, altrimenti avanzo di un altro player
+    // eventualmente i giocatori davanti a me e non contando la casella dove c'è l'altro giocatore, per questo mi basta usare updatePosition
     @Override
     public void play(List<Player> players){
 
@@ -25,25 +24,9 @@ public class OpenSpace extends Card{
             Player giocatore = players.get(i);
             int potenzaMotrice = giocatore.getTruck().getEngineStrength();
 
-            // questo for mi serve per incrementare di uno spazio alla volta la pedina del mio giocatore. Il for annidato serve
-            // a controllare che in quel posto non ci sia nessun altro player, altrimenti fai scorrere di un'altra posizione in avanti la posizione
-            // del giocatore perchè è come se non si fosse mosso di uno spazio vuoto e riporta k a zero perchè adesso devi ricontrollare di nuovo
-            // le posizioni di tutti i giocatori, non essendo la lista di player ordinata
-            for(int j = 0 ; j < potenzaMotrice ; j++){
-                giocatore.updatePosition(1);
+            Utility.updatePosition(players, i , potenzaMotrice);
 
-                //for(int k = 0 ; k < players.size() ; k++){
-                //    if(k != i && players.get(k).getPosition() == giocatore.getPosition()){
-                //        giocatore.updatePosition(1);
-                //        k=0;
-                //    }
-                //}
-                while(thereIsCollision(giocatore, players, i)) {
-                    giocatore.updatePosition(1);
-                }
-            }
             i++;
         }
     }
-
 }
