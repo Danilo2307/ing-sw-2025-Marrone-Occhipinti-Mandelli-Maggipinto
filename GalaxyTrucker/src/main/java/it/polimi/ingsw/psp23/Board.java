4f0fdf4f8cfedfc099e9868ff0a490763015d1af
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-03-20 10:19:02 +0100

    first updates of board

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Board.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/Board.java
@@ -19,7 +19,7 @@ public class Board {
 
     public Board() {
         // per ora istanzio la ship come una 5 x 5, ma la dimensione effettiva sarà da definire
-        ship = new Component[5][5];
+        ship = new Component[5][7];
         garbage = new ArrayList<Component>();
         batteryHubs = new ArrayList<>();
         alienAddOns = new ArrayList<>();
@@ -56,6 +56,52 @@ public class Board {
     }
 
     public void delete(int i, int j) {
+        if(!isValid(i, j) || ship[i][j] == null) {
+            throw new IllegalArgumentException("There isn't any component in this slot or your indexes are invalid: exception in delete(i,j) of Board");
+        }
+        else {
+            // È importante che i "type" in component siano scritti in PascalCase!!!
+            if(ship[i][j].getType().equals("AlienAddOns")) {
+                /* Nelle ArrayList il metodo remove ha due implementazioni, una che riceve in ingresso
+                   l'indice dell'elemento da rimuovere ed una che riceve un oggetto e riceverà la prima
+                   occorrenza dell'oggetto da rimuovere. Io sto usando la seconda.*/
+                /*
+                Inoltre, metto la rimozione in un if nel caso in cui non trovasse l'elemento,
+                il che sarebbe un problema bello peso!!!
+                 */
+                if (!alienAddOns.remove(ship[i][j])) {
+                    System.out.println("Non è stato trovato l'elemento per eliminarlo in 'delete' di Board, controlla la lista di alienAddOns");
+                }
+            }
+            else if(ship[i][j].getType().equals("BatteryHub")) {
+                if(!batteryHubs.remove(ship[i][j])){
+                    System.out.println("Non è stato trovato l'elemento per eliminarlo in 'delete' di Board, controlla la lista di batteryHubs");
+                }
+            }
+            else if(ship[i][j].getType().equals("Container")) {
+
+                if(!containers.remove(ship[i][j])){
+                    System.out.println("Non è stato trovato l'elemento per eliminarlo in 'delete' di Board, controlla la lista di containers");
+                }
+            }
+            else if(ship[i][j].getType().equals("HousingUnit")) {
+                if(!housingUnits.remove(ship[i][j])){
+                    System.out.println("Non è stato trovato l'elemento per eliminarlo in 'delete' di Board, controlla la lista di housingUnits");
+                }
+            }
+            else if(ship[i][j].getType().equals("StructuralComponent")) {
+                if(!structuralComponents.remove(ship[i][j])){
+                    System.out.println("Non è stato trovato l'elemento per eliminarlo in 'delete' di Board, controlla la lista di structuralComponents");
+                }
+            }
+
+            /* Ho aggiunto questa condizione nel caso dovessimo inserire un type in maniera errata
+               di modo che sappiamo dove controllare */
+            else{
+                throw new IllegalArgumentException("There isn't any component like this in the lists: exception in delete(i,j) of Board");
+            }
+            ship[i][j] = null;
+        }
     }
 
     public boolean isFree(int i, int j) {
@@ -102,6 +148,16 @@ public class Board {
     }
 
     public void handleCannonShot(CannonShot cannonShot, int impactLine) {
+        // nel caso in cui sia una cannonata grossa non possiamo fare niente perchè distrugge
+        /*
+        in questo metodo dobbiamo stare attenti al fatto che le colonne nel gioco del livello 2
+        la plancia della nave ha 5 righe(numerate dal 5 al 9) e 7 colonne(numerate da 4 a 10),
+        quindi in questo metodo gestisco prima la conversione di impactLine adattandola agli
+        indici della nostra matrice
+         */
+        if(cannonShot.isBig()){
+            while(!isValid() && )
+        }
     }
 
     public void handleMeteor(Meteor meteor, int impactLine) {
