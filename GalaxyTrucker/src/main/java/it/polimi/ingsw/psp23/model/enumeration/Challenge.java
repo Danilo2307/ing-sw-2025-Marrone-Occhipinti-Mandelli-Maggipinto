8173952c988ee50d1a6477dfa88c8b48f3231012
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-04-01 19:01:05 +0200

    modifica a challenge

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/enumeration/Challenge.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/enumeration/Challenge.java
@@ -3,5 +3,17 @@ package it.polimi.ingsw.psp23.model.enumeration;
     public enum Challenge {
         Cannon_strength,
         Members,
-        Goods
+        Engine_strength;
+
+        public String toString() {
+            if(this == Engine_strength){
+                return "Engine_strength";
+            }
+            else if(this == Cannon_strength){
+                return "Cannon_strength";
+            }
+            else{
+                return "Members";
+            }
+        }
     }
