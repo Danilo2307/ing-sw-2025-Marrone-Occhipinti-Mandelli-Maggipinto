4a53c7d320662a69311e488dcbb9117691288185
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-04-01 16:22:01 +0200

    aggiunta commenti esplicativi in visitor

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/cards/Visitor.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/cards/Visitor.java
@@ -3,10 +3,16 @@ package it.polimi.ingsw.psp23.model.cards;
 // questa è l'interfaccia visitor che conterrà i metodi su cui fare Override nelle varie istanze di visitor
 // ritorna degli oggetti generici perchè, avendo la necessità di tornare sempre tipi diversi, usando i Generics
 // posso usare gli oggetti Wrapper per i tipi primitivi ma possono contenere
-// tutti gli altri tipi che mi servono. Quando creo un visitor che mi ritorna un tipo non primitivo
-// come ad esempio una lista(che possiede quindi dei metodi che possono essere chiamati, motivo per il
+// tutti gli altri tipi che mi servono. Quando creo un visitor(soprattutto quando mi ritorna un tipo non primitivo
+// come ad esempio una lista che possiede quindi dei metodi che possono essere chiamati, motivo per il
 // quale è importante riportare il tipo statico) dovrò creare il visitor specificando il tipo generico
-// tra parentesi angolari, ad esempio:" Visitor<List<List<Item>>> visitor1 = new GetListVisitor();"
+// tra parentesi angolari, ad esempio:" Visitor<List<List<Item>>> visitor1 = new GetListVisitor();",
+// oppure "Visitor<Integer> visitor2 = new getDaysVisitor()" quando so che mi ritornerà un intero.
+// Nel caso di Visitor che mi ritornano "tipi primitiv", quindi ogetti su cui non chiamerò altri
+// metodi come ad esempio "get" si potrebbe anche omettere la specifica del tipo tra parentesi angolari,
+// tuttavia questa operazione prende il nome di "raw usage of parametrized class" e, oltre ad essere rischiosa,
+// rende il codice meno leggibile!!!!
+
 public interface Visitor<T> {
 
     public T visitForPlanets(Planets planets);
