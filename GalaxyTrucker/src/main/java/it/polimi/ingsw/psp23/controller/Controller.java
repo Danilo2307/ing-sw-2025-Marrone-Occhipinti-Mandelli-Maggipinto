f3f492783834711f0e3cb8d896a12f57e16c4b01
Danilo2307 <danilo.marrone@mail.polimi.it>
2025-04-11 16:59:59 +0200

    Spostato controller in cartella principale

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/controller/Controller.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/controller/Controller.java
@@ -8,7 +8,7 @@ import it.polimi.ingsw.psp23.model.components.HousingUnit;
 import it.polimi.ingsw.psp23.model.enumeration.GameStatus;
 import it.polimi.ingsw.psp23.model.enumeration.Side;
 
-public class Controller{
+public class Controller {
     private Game game; //inizializzo il model
     private CardHandler cardHandler;
     private Timer timer;
@@ -24,16 +24,16 @@ public class Controller{
     }
 
     public void addPlayerToGame(String nickname) throws PlayerExistsException, GameFullException {
-            if (game.getPlayers().size() < 4)
-                game.addPlayer(nickname);
-            else
-                throw new GameFullException("The game is full");
+        if (game.getPlayers().size() < 4)
+            game.addPlayer(nickname);
+        else
+            throw new GameFullException("The game is full");
     }
 
-    public void startBuildingPhase(){
+    public void startBuildingPhase() {
 
         for (Player player : game.getPlayers()) {
-            player.getTruck().addComponent(new HousingUnit(Side.UNIVERSAL_CONNECTOR,Side.UNIVERSAL_CONNECTOR,Side.UNIVERSAL_CONNECTOR,Side.UNIVERSAL_CONNECTOR,true),2,3);
+            player.getTruck().addComponent(new HousingUnit(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, true), 2, 3);
         }//questo for inizializza la cabina centrale dei player con la prima housing unit
 
         game.setGameStatus(GameStatus.Building);
@@ -41,59 +41,60 @@ public class Controller{
         startTimer();
     }
 
-    public void startTimer(){
+    public void startTimer() {
         //la clessidra dura un minuto e mezzo
-        timer.startCountdown(90,this:: handleTimeout);
+        timer.startCountdown(90, this::handleTimeout);
     }
 
 
-
-    public void handleTimeout(){
-        if(!isFirstBuildingPhaseEnded) {
+    public void handleTimeout() {
+        if (!isFirstBuildingPhaseEnded) {
             isFirstBuildingPhaseEnded = true;
             System.out.println("First building phase ended\n");
-        }else{
+        } else {
             startCheckBoard();
         }
 
 
     }
 
-    public void startCheckBoard() throws IllegalTruckException{
+    public void startCheckBoard() throws IllegalTruckException {
 
-        if(game.getGameStatus() == GameStatus.Building) {
+        if (game.getGameStatus() == GameStatus.Building) {
             timer.shutdown();
             game.setGameStatus(GameStatus.CheckBoards);
         }
 
         for (Player player : game.getPlayers()) {
-            if(!player.getTruck().check()){
+            if (!player.getTruck().check()) {
                 System.out.println("Player " + player.getNickname() + " has an illegal truck\n");
-                throw new IllegalTruckException(player.getNickname()+" has an illegal truck");
+                throw new IllegalTruckException(player.getNickname() + " has an illegal truck");
             }
         }
 
     }
 
     public void addComponent(String nickname, Component c, int x, int y) {
-        game.getPlayerFromNickname(nickname).getTruck().addComponent(c,x,y);
+        game.getPlayerFromNickname(nickname).getTruck().addComponent(c, x, y);
     }
 
-    public void playerFinishedBuilding(String nickname){
-        switch(currentPosition){
+    public void playerFinishedBuilding(String nickname) {
+        switch (currentPosition) {
             case 1:
                 game.setCurrentPlayer(game.getPlayerFromNickname(nickname));
                 game.getPlayerFromNickname(nickname).setPosition(8);
-
-
-
-
-
-
-
-
-
-
-
-
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
