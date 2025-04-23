00bd1620a73d8418fe02bae7bce09dff58b58ebc
Federico Mandelli <10827992@polimi.it>
2025-04-23 13:56:16 +0200

    creazione package view

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/TUI/TuiApplication.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.psp23.view.TUI;
+
+public class TuiApplication {
+
+    //////
+
+}
---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/prova.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.psp23.view;
+
+public class prova {
+    // classe necessario se no non creava package view.TUI innestato
+}
