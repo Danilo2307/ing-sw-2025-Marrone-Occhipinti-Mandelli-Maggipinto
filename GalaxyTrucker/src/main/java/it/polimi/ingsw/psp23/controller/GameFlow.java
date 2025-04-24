4e7ade8cc56f79eb1b74294130c90c08142caad9
gigimaggi03 <luigi.maggipinto@mail.polimi.it>
2025-04-24 19:45:09 +0200

    GameFlow created

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/controller/GameFlow.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/controller/GameFlow.java
@@ -1,4 +1,67 @@
 package it.polimi.ingsw.psp23.controller;
 
-public class GameFlow {
+import it.polimi.ingsw.psp23.Player;
+import it.polimi.ingsw.psp23.model.Game.Game;
+import it.polimi.ingsw.psp23.model.cards.Card;
+import it.polimi.ingsw.psp23.model.enumeration.GameStatus;
+
+import java.io.Serializable;
+
+public class GameFlow implements Serializable {
+
+    private GameStatus currentState;
+    private int turn;
+    private Card currentCard;
+    private Player currentPlayer;
+
+    public GameFlow() {
+        currentState = GameStatus.Building;
+        turn = 0;
+        currentCard = null;
+        currentPlayer = null;
+    }
+
+    public Card nextCard(){
+        Game.getInstance().sortPlayersByPosition();
+        if(Game.getInstance().getNextCard() != null || !Game.getInstance().getPlayers().isEmpty()){
+            currentCard = Game.getInstance().getNextCard();
+            turn = 0;
+            return currentCard;
+        }
+        else {
+            Game.getInstance().setGameStatus(GameStatus.End);
+            Game.getInstance().calculateFinalScores();
+        }
+    }
+
+    public Player nextTurn(){
+        turn++;
+        if(turn > Game.getInstance().getPlayers().size()){
+            Game.getInstance().sortPlayersByPosition();
+            nextCard();
+            currentPlayer = leaderPlayer();
+        }
+        else{
+            currentPlayer = Game.getInstance().getNextPlayer();
+        }
+        return currentPlayer;
+    }
+
+    public Player currentPlayer(){
+        return Game.getInstance().getCurrentPlayer();
+    }
+
+    public Player leaderPlayer(){
+        return Game.getInstance().getPlayers().getFirst();
+    }
+
+
+    public GameStatus getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(GameStatus currentState) {
+        this.currentState = currentState;
+    }
 }
+
