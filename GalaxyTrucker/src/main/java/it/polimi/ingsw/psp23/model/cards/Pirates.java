8dc8df73f0b8d904a5243f32daed7a9eb774030e
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-04-26 12:30:50 +0200

    prima bozza play di pirati

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/cards/Pirates.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/cards/Pirates.java
@@ -1,5 +1,11 @@
 package it.polimi.ingsw.psp23.model.cards;
 
+import it.polimi.ingsw.psp23.Board;
+import it.polimi.ingsw.psp23.Player;
+import it.polimi.ingsw.psp23.Utility;
+import it.polimi.ingsw.psp23.model.Game.Game;
+import it.polimi.ingsw.psp23.model.components.Cannon;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +40,44 @@ public class Pirates extends Card {
     @Override
     public Object call(Visitor visitor){
         return visitor.visitForPirates(this);
+    }
+
+    public void play(InputObject input){
+
+        // Devo prima attivare tutti i cannoni che mi sono passati nell'inputObject tramite il metodo activateCannon
+        // così che poi il metodo calculateCannonStrength calcoli correttamente la potenza di fuoco
+        Player currentPlayer = Game.getInstance().getCurrentPlayer();
+        Board board = currentPlayer.getTruck();
+        List<Cannon> cannoni = board.getCannons();
+        double potenzaDiFuocoPlayer;
+
+        for(Integer[] i : input.getCannons()){
+            for(Cannon c : cannoni){
+                if(c.getX() == i[0] && c.getY() == i[1]){
+                    c.activeCannon();
+                    break;
+                }
+            }
+        }
+        potenzaDiFuocoPlayer = board.calculateCannonStrength();
+
+        // Adesso faccio il confronto tra la potenza di fuoco del player e quella dei pirates per vedere se vince il player
+        if(potenzaDiFuocoPlayer > firepower){
+            if(input.getDecision()){
+                Utility.updatePosition(Game.getInstance().getPlayers(), Game.getInstance().getCurrentPlayerIndex(), -days);
+                currentPlayer.updateMoney(prize);
+            }
+        }
+        else if(potenzaDiFuocoPlayer == firepower){
+            // Qui non bisogna fare niente perchè i pirati attaccano i player presenti dopo
+        }
+        else{
+            // In questo caso dovremmo aspettare che tutti i giocatori abbiano affrontato i pirati o che questi ultimi
+            // siano stati sconfitti per chiedere al current player di lanciare i dadi e poi fare arrivare le cannonate
+            // a tutti gli altri player. Per fare ciò io metterei uno stato in cui si ci arriva o dopo aver sconfitto
+            // i nemici o dopo avere perso e, quando tutti i player sono in quello stato allora si comincia a gestire
+            // le cannonate
+        }
     }
 
 }
