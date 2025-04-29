1fc3a7d27454c47d0b46fdc35fa4be376d6b7e8c
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-04-29 19:04:00 +0200

    modifica action

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/protocol/request/Action.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/protocol/request/Action.java
@@ -1,8 +1,5 @@
 package it.polimi.ingsw.psp23.protocol.request;
 
-import it.polimi.ingsw.psp23.protocol.response.EventVisitor;
-import it.polimi.ingsw.psp23.view.TUI.TuiApplication;
-
 import java.io.Serializable;
 
 public interface Action extends Serializable {
