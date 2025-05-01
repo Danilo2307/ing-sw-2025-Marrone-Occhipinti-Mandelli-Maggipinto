22ad68d1614368308474b4bcc51563f565900ff6
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-05-01 09:08:57 +0200

    eliminazione inputobject

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/cards/VisitorInput.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/cards/VisitorInput.java
@@ -2,26 +2,26 @@ package it.polimi.ingsw.psp23.model.cards;
 
 public interface VisitorInput<T> {
 
-    public T visitForPlanets(Planets planets, InputObject inputObject);
+    public T visitForPlanets(Planets planets);
 
-    public T visitForAbandonedShip(AbandonedShip abandonedShip, InputObject inputObject);
+    public T visitForAbandonedShip(AbandonedShip abandonedShip);
 
-    public T visitForAbandonedStation(AbandonedStation abandonedStation, InputObject inputObject);
+    public T visitForAbandonedStation(AbandonedStation abandonedStation);
 
-    public T visitForCombatZone(CombatZone combatZone, InputObject inputObject);
+    public T visitForCombatZone(CombatZone combatZone);
 
-    public T visitForEpidemic(Epidemic epidemic, InputObject inputObject);
+    public T visitForEpidemic(Epidemic epidemic);
 
-    public T visitForMeteorSwarm(MeteorSwarm meteorSwarm, InputObject inputObject);
+    public T visitForMeteorSwarm(MeteorSwarm meteorSwarm);
 
-    public T visitForOpenSpace(OpenSpace openSpace, InputObject inputObject);
+    public T visitForOpenSpace(OpenSpace openSpace);
 
-    public T visitForPirates(Pirates pirates, InputObject inputObject);
+    public T visitForPirates(Pirates pirates);
 
-    public T visitForSlavers(Slavers slavers, InputObject inputObject);
+    public T visitForSlavers(Slavers slavers);
 
-    public T visitForSmugglers(Smugglers smugglers, InputObject inputObject);
+    public T visitForSmugglers(Smugglers smugglers);
 
-    public T visitForStardust(Stardust stardust, InputObject inputObject);
+    public T visitForStardust(Stardust stardust);
 
 }
