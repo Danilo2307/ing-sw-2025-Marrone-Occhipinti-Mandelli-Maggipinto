ef38061a8085f4671291e0a341351c9a22ee5d98
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-05-05 12:23:01 +0200

    avvio partita corretto

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/MainServer.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/MainServer.java
@@ -21,8 +21,7 @@ public class MainServer {
 
         Users.getInstance().createClientHandler(connectionId);
         Server.getInstance().sendMessage(new DirectMessage(new RequestNumPlayers()), connectionId);
-        Message receivedMessage = Server.getInstance().receiveMessage(connectionId);
-        Users.getInstance().getClientHandler(connectionId).handleMessage(receivedMessage);
+
 
         Server.getInstance().setServerSocket("localhost", 8000);
 
