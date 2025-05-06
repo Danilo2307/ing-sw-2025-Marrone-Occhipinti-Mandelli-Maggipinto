f17cd30c5199be770bc9e9b01ea6234af591ee36
Danilo2307 <danilo.marrone@mail.polimi.it>
2025-05-06 16:43:59 +0200

    corrections

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/cards/Planets.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/cards/Planets.java
@@ -233,22 +233,26 @@ public class Planets extends Card {
 
     @Override
     public String toString(){
+
         if(planetGoods.size() == 2){
             return
-                    "I pianeti sono 2\n" +
+                    "è uscita la carta Planets\n" +
+                            "I pianeti sono 2\n" +
                             "● ->" + planetGoods.get(0).toString() +"\n" +
                             "● ->" + planetGoods.get(1).toString() +"\n" +
                             "i giorni persi sono " + getDaysLost();
         }else if(planetGoods.size() == 3){
             return
-                    "I pianeti sono 3\n" +
+                    "è uscita la carta Planets\n" +
+                            "I pianeti sono 3\n" +
                             "● ->" + planetGoods.get(0).toString() +"\n" +
                             "● ->" + planetGoods.get(1).toString() +"\n" +
                             "● ->" + planetGoods.get(2).toString() +"\n" +
                             "i giorni persi sono " + getDaysLost();
         }else if(planetGoods.size() == 4){
             return
-                    "I pianeti sono 4\n" +
+                    "è uscita la carta Planets\n" +
+                            "I pianeti sono 4\n" +
                             "● ->" + planetGoods.get(0).toString() +"\n" +
                             "● ->" + planetGoods.get(1).toString() +"\n" +
                             "● ->" + planetGoods.get(2).toString() +"\n" +
@@ -256,7 +260,8 @@ public class Planets extends Card {
                             "i giorni persi sono " + getDaysLost();
         }else if(planetGoods.size() == 5){
             return
-                    "I pianeti sono 5\n" +
+                    "è uscita la carta Planets" +
+                            "I pianeti sono 5\n" +
                             "● ->" + planetGoods.get(0).toString() +"\n" +
                             "● ->" + planetGoods.get(1).toString() +"\n" +
                             "● ->" + planetGoods.get(2).toString() +"\n" +
