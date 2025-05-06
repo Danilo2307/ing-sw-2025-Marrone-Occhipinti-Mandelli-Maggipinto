645c8a336ad6987c0c6061f5e76d22c1c58edce8
Federico Mandelli <10827992@polimi.it>
2025-05-06 11:48:56 +0200

    azione prendi mazzetto

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/protocol/request/TakeVisibleDeck.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.psp23.protocol.request;
+
+import it.polimi.ingsw.psp23.exceptions.InvalidActionException;
+import it.polimi.ingsw.psp23.model.Game.Game;
+import it.polimi.ingsw.psp23.model.Game.Player;
+import it.polimi.ingsw.psp23.model.cards.Card;
+import it.polimi.ingsw.psp23.network.messages.DirectMessage;
+import it.polimi.ingsw.psp23.network.socket.Server;
+import it.polimi.ingsw.psp23.protocol.response.ShowVisibleDeck;
+import it.polimi.ingsw.psp23.protocol.response.StringResponse;
+
+import java.util.ArrayList;
+
+public record TakeVisibleDeck(int index) implements Action{
+
+    public void handle(String username) {
+        Game game = Game.getInstance();
+        Player player = game.getPlayerFromNickname(username);
+        ArrayList<Card> deckChosen = switch(index) {
+            case 1 -> game.getVisibleDeck1(player);
+            case 2 -> game.getVisibleDeck2(player);
+            case 3 -> game.getVisibleDeck3(player);
+            default -> throw new InvalidActionException("Valore inaspettato: " + index);
+        };
+
+        if (deckChosen == null) {
+            DirectMessage dm = new DirectMessage(new StringResponse("Il mazzetto "+index+" che hai richiesto è stato già preso! Attendi che il proprietario lo rilasci"))
+            Server.getInstance().sendMessage(username, dm);
+        }
+        else {
+            DirectMessage dm = new DirectMessage(new ShowVisibleDeck(deckChosen));
+            Server.getInstance().sendMessage(username, dm);
+        }
+    }
+
+
+    @Override
+    public <T> T call(ActionVisitor<T> actionVisitor, String username){
+        return actionVisitor.visitForTakeVisibleDeck(this, username);
+    }
+
+    @Override
+    public <T> T call(ActionVisitorSinglePar<T> actionVisitorSinglePar){
+        return null;
+    }
+
+
+
+
+}
