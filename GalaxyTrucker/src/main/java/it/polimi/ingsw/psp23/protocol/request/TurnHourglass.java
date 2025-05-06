62f8640dcf6f4e0890a0eae75fafff4c7eddc4f9
Danilo2307 <danilo.marrone@mail.polimi.it>
2025-05-06 11:08:53 +0200

    timer in actions

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/protocol/request/TurnHourglass.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/protocol/request/TurnHourglass.java
@@ -1,10 +1,12 @@
 package it.polimi.ingsw.psp23.protocol.request;
 
+import it.polimi.ingsw.psp23.controller.Controller;
+
 /** event triggered when a user wants to turn the hourglass during the building phase */
 public record TurnHourglass() implements Action {
 
     public void handle(String username){
-
+        Controller.getInstance().startTimer();
     }
 
     @Override
