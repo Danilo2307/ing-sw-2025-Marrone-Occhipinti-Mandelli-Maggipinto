e04ab0d42f30e233e570c63cf11c70a5717d42c2
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-05-08 14:49:34 +0200

    visualizzazione automatica della nave dopo aver fatto la rimozione di un tile e visualizzazione automatica delle info della tile dopo averla ruotata e dopo aver modificato i componenti di una cabina

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/protocol/request/RotateTile.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/protocol/request/RotateTile.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.psp23.protocol.request;
 
 import it.polimi.ingsw.psp23.model.Game.Player;
 import it.polimi.ingsw.psp23.model.Game.Game;
+import it.polimi.ingsw.psp23.network.messages.DirectMessage;
+import it.polimi.ingsw.psp23.network.socket.Server;
+import it.polimi.ingsw.psp23.protocol.response.TileResponse;
 
 /** the user wants to rotate (senso orario di 90 gradi) the tile currently in hand*/
 public record RotateTile() implements Action {
@@ -11,6 +14,7 @@ public record RotateTile() implements Action {
         Game game = Game.getInstance();
         Player p = game.getPlayerFromNickname(username);
         p.rotateTileInHand();
+        Server.getInstance().sendMessage(username, new DirectMessage(new TileResponse(p.getCurrentTileInHand())));
     }
 
     @Override
