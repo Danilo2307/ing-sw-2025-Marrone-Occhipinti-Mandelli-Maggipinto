bf98795ed567a27c7b9331fe7e8575c9046727f8
Alberto Occhipinti <alberto.occhipinti@mail.polimi.it>
2025-05-10 17:42:37 +0200

    nuovo updateposition

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/Game/Utility.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/Game/Utility.java
@@ -7,18 +7,55 @@ public final class Utility {
     private static final Random rand = new Random();
 
     // evito istanziazione
-    private Utility() {}
+    private Utility() {
+    }
 
     public static int roll2to12() {
         return rand.nextInt(6) + 1 + rand.nextInt(6) + 1;
     }
 
-    public static int randomComponent(int size){
-        return rand.nextInt(0, size );
+    public static int randomComponent(int size) {
+        return rand.nextInt(0, size);
     }
 
+    public static void updatePosition(List<Player> players, int playerIndex, int positionsToJump) {
+        Player giocatore = players.get(playerIndex);
+        int playerLocation = giocatore.getPosition();
+        int saltiEffettivi = positionsToJump;
+        if (positionsToJump > 0) {
+            for (int offset = 0; offset < saltiEffettivi; offset++) {
+                for (int k = 0; k < players.size(); k++) {
+                    if (k != playerIndex && players.get(k).getPosition() == playerLocation + offset) {
+                        saltiEffettivi++;
+                        k = 0; // azzerando k ottengo l'effetto di far ripartire il ciclo nel caso in cui ci sia una collisione, in modo da
+                        // essere sicuro di confrontare il giocatore corrente con TUTTI GLI ALTRI GIOCATORI PRESENTI
+
+                        /* TODO: bisogna attenzionare che non ci siano loop infiniti dovuto magari ad un accavallamento successivo delle pedine
+                                 che potrebbe portare questo ciclo a non esaurirsi mai perchè trova sempre posizioni occupate dopo */
+                    }
+                }
+            }
+        } else {
+            for (int offset = 0; offset > saltiEffettivi; offset--) {
+                for (int k = 0; k < players.size(); k++) {
+                    if (k != playerIndex && players.get(k).getPosition() == playerLocation + offset) {
+                        saltiEffettivi--;
+                        k = 0; // azzerando k ottengo l'effetto di far ripartire il ciclo nel caso in cui ci sia una collisione, in modo da
+                        // essere sicuro di confrontare il giocatore corrente con TUTTI GLI ALTRI GIOCATORI PRESENTI
+
+                        /* TODO: bisogna attenzionare che non ci siano loop infiniti dovuto magari ad un accavallamento successivo delle pedine
+                                 che potrebbe portare questo ciclo a non esaurirsi mai perchè trova sempre posizioni occupate dopo */
+                    }
+                }
+            }
+        }
+        giocatore.setPosition(saltiEffettivi);
+    }
+
+}
+
     // È importante che il positionsToJump sia negativo quando si arretra!!!
-    public static void updatePosition(List<Player> players,int playerIndex, int positionsToJump){
+    /*public static void updatePosition(List<Player> players,int playerIndex, int positionsToJump){
         Player giocatore = players.get(playerIndex);
         int playerLocation = giocatore.getPosition();
         int newlocation = positionsToJump + playerLocation;
@@ -34,15 +71,15 @@ public final class Utility {
                     k = 0; // azzerando k ottengo l'effetto di far ripartire il ciclo nel caso in cui ci sia una collisione, in modo da
                            // essere sicuro di confrontare il giocatore corrente con TUTTI GLI ALTRI GIOCATORI PRESENTI
 
-                    /* TODO: bisogna attenzionare che non ci siano loop infiniti dovuto magari ad un accavallamento successivo delle pedine
-                             che potrebbe portare questo ciclo a non esaurirsi mai perchè trova sempre posizioni occupate dopo */
+                    *//* TODO: bisogna attenzionare che non ci siano loop infiniti dovuto magari ad un accavallamento successivo delle pedine
+                             che potrebbe portare questo ciclo a non esaurirsi mai perchè trova sempre posizioni occupate dopo *//*
                 }
             }
             giocatore.setPosition(positionsToJump);
     }
-    /* TODO: la check per verificare se un giocatore è stato doppiato e quindi dovrebbe abbandonare il gioco, la lascio ad un'altra funzione
-             ma in caso, dovrebbe essere inserita qui, alla fine dell'aggiornamento delle posizioni*/
-}
+    *//* TODO: la check per verificare se un giocatore è stato doppiato e quindi dovrebbe abbandonare il gioco, la lascio ad un'altra funzione
+             ma in caso, dovrebbe essere inserita qui, alla fine dell'aggiornamento delle posizioni*//*
+}*/
 
 
 /* qui sono presenti i metodi thereIsCollision ed il metodo alternativo per scorrere le posizioni
