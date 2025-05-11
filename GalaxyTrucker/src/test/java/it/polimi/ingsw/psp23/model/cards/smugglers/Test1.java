43bc8f05b94b9a8ef07909fc48d2b8a1dad807af
gigimaggi03 <luigi.maggipinto@mail.polimi.it>
2025-05-11 18:22:45 +0200

    SetUp test1 smugglers

---
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/psp23/model/cards/smugglers/Test1.java
@@ -0,0 +1,136 @@
+package it.polimi.ingsw.psp23.model.cards.smugglers;
+
+import it.polimi.ingsw.psp23.model.Game.Game;
+import it.polimi.ingsw.psp23.model.Game.Item;
+import it.polimi.ingsw.psp23.model.Game.Player;
+import it.polimi.ingsw.psp23.model.cards.AbandonedStation;
+import it.polimi.ingsw.psp23.model.cards.Smugglers;
+import it.polimi.ingsw.psp23.model.components.*;
+import it.polimi.ingsw.psp23.model.enumeration.Color;
+import it.polimi.ingsw.psp23.model.enumeration.Side;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+    //TEST A PAGINA 14 MANUALE GALAXY TRUCKER
+    Game game;
+    Player p1, p2, p3;
+    Smugglers card;
+
+    @BeforeEach
+    void setUp() {
+        this.game = Game.getInstance();
+
+        game.addPlayer("Albi");
+        game.addPlayer("Fede");
+        game.addPlayer("Gigi");
+
+        p1 = game.getPlayerFromNickname("Albi");
+        p2 = game.getPlayerFromNickname("Fede");
+        p3 = game.getPlayerFromNickname("Gigi");
+
+
+        //CABINE CENTRALI
+        HousingUnit ha1 = new HousingUnit(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, true);
+        ha1.moveToHand();
+        HousingUnit hf1 = new HousingUnit(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, true);
+        hf1.moveToHand();
+        HousingUnit hg1 = new HousingUnit(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, true);
+        hg1.moveToHand();
+        p1.getTruck().addComponent(ha1, 2, 3);
+        p2.getTruck().addComponent(hf1, 2, 3);
+        p3.getTruck().addComponent(hg1, 2, 3);
+
+        //BOARD ALBI
+        Container ca1 = new Container(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, 3, Color.Blue, new ArrayList<>());
+        ca1.moveToHand();
+        p1.getTruck().addComponent(ca1, 3, 3);
+        BatteryHub ba1 = new BatteryHub(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, 2);
+        ba1.moveToHand();
+        p1.getTruck().addComponent(ba1, 2, 2);
+        Cannon caa1 = new Cannon(Side.GUN, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, false);
+        caa1.moveToHand();
+        p1.getTruck().addComponent(caa1, 4, 3);
+        Cannon caa2 = new Cannon(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.GUN, true);
+        caa2.moveToHand();
+        p1.getTruck().addComponent(caa2, 2, 4);
+        Cannon caa3 = new Cannon(Side.GUN, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, false);
+        caa3.moveToHand();
+        p1.getTruck().addComponent(caa3, 1, 2);
+        Cannon caa4 = new Cannon(Side.GUN, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, true);
+        caa4.moveToHand();
+        p1.getTruck().addComponent(caa4, 1, 4);
+        Container ca2 = new Container(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, 1, Color.Red, new ArrayList<>());
+        ca2.moveToHand();
+        p1.getTruck().addComponent(ca2, 3, 4);
+        Engine ea1 = new Engine(Side.UNIVERSAL_CONNECTOR, Side.ENGINE, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, false);
+        ea1.moveToHand();
+        p1.getTruck().addComponent(ea1, 3, 2);
+
+        Item ia1 = new Item(Color.Yellow);
+        ca1.loadItem(ia1);
+        Item ia2 = new Item(Color.Yellow);
+        ca1.loadItem(ia2);
+        Item ia3 = new Item(Color.Blue);
+        ca1.loadItem(ia3);
+        Item ia4 = new Item(Color.Red);
+        ca2.loadItem(ia4);
+
+        //BOARD FEDE
+        Cannon caf1 = new Cannon(Side.GUN, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, true);
+        caf1.moveToHand();
+        p2.getTruck().addComponent(caf1, 1, 3);
+        BatteryHub bf1 = new BatteryHub(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, 3);
+        bf1.moveToHand();
+        p2.getTruck().addComponent(bf1, 2, 4);
+        Container cf1 = new Container(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, 2, Color.Blue, new ArrayList<>());
+        cf1.moveToHand();
+        p2.getTruck().addComponent(cf1, 2, 2);
+        Engine ef1 = new Engine(Side.UNIVERSAL_CONNECTOR, Side.ENGINE, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, false);
+        ef1.moveToHand();
+        p2.getTruck().addComponent(ef1, 3, 3);
+        Cannon caf2 = new Cannon(Side.GUN, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, true);
+        caf2.moveToHand();
+        p2.getTruck().addComponent(caf2, 1, 4);
+        Cannon caf3 = new Cannon(Side.UNIVERSAL_CONNECTOR, Side.GUN, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, true);
+        caf3.moveToHand();
+        p2.getTruck().addComponent(caf3, 3, 4);
+        BatteryHub bf2 = new BatteryHub(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, 2);
+        bf2.moveToHand();
+        p2.getTruck().addComponent(bf2, 1, 4);
+
+        //BOARD GIGI
+        HousingUnit hg2 = new HousingUnit(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, false);
+        hg2.moveToHand();
+        p3.getTruck().addComponent(hg2, 3, 3);
+        BatteryHub bg1 = new BatteryHub(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, 3);
+        bg1.moveToHand();
+        p3.getTruck().addComponent(bg1, 2, 2);
+        Container cg1 = new Container(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, 2, Color.Red, new ArrayList<>());
+        cg1.moveToHand();
+        p3.getTruck().addComponent(cf1, 2, 4);
+        Cannon cag1 = new Cannon(Side.GUN, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, true);
+        cag1.moveToHand();
+        p3.getTruck().addComponent(cag1, 1, 3);
+        Cannon cag2 = new Cannon(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.GUN, Side.UNIVERSAL_CONNECTOR, false);
+        cag2.moveToHand();
+        p3.getTruck().addComponent(cag2, 1, 2);
+        Cannon cag3 = new Cannon(Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, Side.GUN, false);
+        cag3.moveToHand();
+        p3.getTruck().addComponent(cag3, 1, 4);
+        Engine eg1 = new Engine(Side.UNIVERSAL_CONNECTOR, Side.ENGINE, Side.UNIVERSAL_CONNECTOR, Side.UNIVERSAL_CONNECTOR, false);
+        eg1.moveToHand();
+        p3.getTruck().addComponent(eg1, 3, 2);
+
+        //SET POS INIZIALI
+        p1.setPosition(12);
+        p2.setPosition(10);
+        p3.setPosition(8);
+        game.sortPlayersByPosition();
+
+        card = new Smugglers(1, 4, 2, 1, List.of(new Item(Color.Yellow), new Item(Color.Green), new Item(Color.Blue)));
+    }
+}
+
