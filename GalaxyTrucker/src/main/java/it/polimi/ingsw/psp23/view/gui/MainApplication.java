74259ce5e75211006aac10f411382953cc62c75b
Danilo2307 <danilo.marrone@mail.polimi.it>
2025-05-12 11:16:25 +0200

    prove gui

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/gui/MainApplication.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/gui/MainApplication.java
@@ -9,15 +9,12 @@ import javafx.stage.Stage;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        // 1) Carica il file FXML (attenzione al path: risorsa sotto resources/)
+
         FXMLLoader loader = new FXMLLoader(
                 getClass().getResource("/fxml/hello-view.fxml")
         );
         Parent root = loader.load();
 
-        // 2) Se serve, puoi ottenere il controller così:
-        // HelloController controller = loader.getController();
-        // controller.initData(...);
 
         Scene scene = new Scene(root, 400, 300);
         stage.setTitle("Galaxy Trucker");
