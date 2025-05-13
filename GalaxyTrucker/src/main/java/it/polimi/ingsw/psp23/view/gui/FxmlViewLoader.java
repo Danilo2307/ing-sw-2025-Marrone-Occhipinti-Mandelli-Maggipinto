67d76ef1f04da2e9a4b882aaae019615aef118ed
Danilo2307 <danilo.marrone@mail.polimi.it>
2025-05-13 15:28:43 +0200

    push di commenti sulle classi della gui (non finiti)

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/gui/FxmlViewLoader.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/gui/FxmlViewLoader.java
@@ -1,4 +1,6 @@
 package it.polimi.ingsw.psp23.view.gui;
-//classe utility per caricare file FXML e i loro controller specifici
+//classe utility per associare file FXML e i loro controller specifici
+//quindi trova i file FXML, crea l'istanza del controller giusto
+//carica l'albero di nodi definito nell'fxml e restituisce il nodo radice
 public class FxmlViewLoader {
 }
