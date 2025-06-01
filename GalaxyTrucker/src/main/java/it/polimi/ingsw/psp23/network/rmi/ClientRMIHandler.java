0a579d634207d3f7e1b31e6682ca6c7af5e5458e
Federico Mandelli <10827992@polimi.it>
2025-06-01 16:39:10 +0200

    drag drop rmi fix

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/network/rmi/ClientRMIHandler.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/network/rmi/ClientRMIHandler.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.psp23.network.rmi;
 
 import it.polimi.ingsw.psp23.controller.Controller;
 import it.polimi.ingsw.psp23.exceptions.GameException;
+import it.polimi.ingsw.psp23.exceptions.InvalidCoordinatesException;
 import it.polimi.ingsw.psp23.exceptions.LobbyUnavailableException;
 import it.polimi.ingsw.psp23.model.Game.Game;
 import it.polimi.ingsw.psp23.model.Game.Player;
@@ -9,9 +10,11 @@ import it.polimi.ingsw.psp23.network.UsersConnected;
 import it.polimi.ingsw.psp23.network.messages.BroadcastMessage;
 import it.polimi.ingsw.psp23.network.messages.DirectMessage;
 import it.polimi.ingsw.psp23.network.messages.Message;
+import it.polimi.ingsw.psp23.network.socket.Server;
 import it.polimi.ingsw.psp23.protocol.request.Action;
 import it.polimi.ingsw.psp23.protocol.request.HandleActionVisitor;
 import it.polimi.ingsw.psp23.protocol.response.ErrorResponse;
+import it.polimi.ingsw.psp23.protocol.response.IncorrectWelding;
 import it.polimi.ingsw.psp23.protocol.response.SelectLevel;
 
 import java.rmi.RemoteException;
@@ -137,6 +140,12 @@ public class ClientRMIHandler extends UnicastRemoteObject implements ClientRMIHa
         catch(GameException e) {
             DirectMessage dm = new DirectMessage(new ErrorResponse(e.getMessage()));
             sendToUser(nameConnection, dm);
+        }
+        catch (InvalidCoordinatesException invalidCoordinatesException) {
+            DirectMessage dm = new DirectMessage(new ErrorResponse(invalidCoordinatesException.getMessage()));
+            Server.getInstance().sendMessage(username, dm);
+            DirectMessage dm1 = new DirectMessage(new IncorrectWelding());
+            Server.getInstance().sendMessage(username, dm1);
         }
     }
 
