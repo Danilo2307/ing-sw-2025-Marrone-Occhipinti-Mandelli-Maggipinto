5f9d1b6aa872aca274c157380fa9502f61f2c91f
Danilo2307 <danilo.marrone@mail.polimi.it>
2025-06-06 14:41:43 +0200

    volo di prova completamente finito

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/gui/guicontrollers/FlightBoardController0.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/gui/guicontrollers/FlightBoardController0.java
@@ -1,24 +1,14 @@
 package it.polimi.ingsw.psp23.view.gui.guicontrollers;
-
 import it.polimi.ingsw.psp23.network.Client;
-import it.polimi.ingsw.psp23.protocol.request.ReleaseDeck;
-import it.polimi.ingsw.psp23.protocol.request.RequestFlightBoard;
-import it.polimi.ingsw.psp23.protocol.request.TakeVisibleDeck;
 import it.polimi.ingsw.psp23.view.gui.GuiApplication;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import java.awt.*;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.Objects;
-
 import it.polimi.ingsw.psp23.model.enumeration.Color;
-import javafx.stage.Stage;
 
 public class FlightBoardController0 {
     @FXML private Button backToShip;
@@ -40,12 +30,6 @@ public class FlightBoardController0 {
     @FXML private ImageView position15;
     @FXML private ImageView position16;
     @FXML private ImageView position17;
-    @FXML private ImageView position18;
-    @FXML private ImageView position19;
-    @FXML private ImageView position20;
-    @FXML private ImageView position21;
-    @FXML private ImageView position22;
-    @FXML private ImageView position23;
     private Client client;
     private ImageView[] positions;
 
@@ -53,8 +37,7 @@ public class FlightBoardController0 {
         positions = new ImageView[]{
                 position0, position1, position2, position3, position4, position5,
                 position6, position7, position8, position9, position10, position11,
-                position12, position13, position14, position15, position16, position17,
-                position18, position19, position20, position21, position22, position23
+                position12, position13, position14, position15, position16, position17
         };
     }
 
