726c8b66cf4f9a983e90ef65ccea31a56190fe91
Federico Mandelli <10827992@polimi.it>
2025-06-25 22:53:30 +0200

    javadoc model finito

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/helpers/CannonShot.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/model/helpers/CannonShot.java
@@ -1,6 +1,13 @@
 package it.polimi.ingsw.psp23.model.helpers;
 import it.polimi.ingsw.psp23.model.enumeration.Direction;
 
+/**
+ * Represents a single shot fired from a cannon, with properties indicating
+ * the size of the shot and the direction in which it is fired.
+ *
+ * Instances of this class are immutable, meaning that their state
+ * cannot be changed after they are created.
+ */
 public class CannonShot {
     private final boolean big;
     private final Direction direction;
