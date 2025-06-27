595736366d22c1eaa71c0eb95741a0c61ff1973e
Federico Mandelli <10827992@polimi.it>
2025-06-27 17:09:48 +0200

    cazzzatine

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/gui/guicontrollers/DeckViewController.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/view/gui/guicontrollers/DeckViewController.java
@@ -16,9 +16,6 @@ public class DeckViewController {
     private int deck = 0;
 
 
-
-
-
     @FXML
     public void onBackToFlightBoardClicked(){
         int deck = GuiApplication.getInstance().getDeckNumber();
