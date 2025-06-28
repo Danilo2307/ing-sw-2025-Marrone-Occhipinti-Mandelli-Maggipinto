d1fb253cb8d0d09eecdf055c09295890fe82a6e2
gigimaggi03 <luigi.maggipinto@mail.polimi.it>
2025-06-28 11:22:58 +0200

    End

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/network/rmi/ClientCallback.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/psp23/network/rmi/ClientCallback.java
@@ -33,7 +33,9 @@ public class ClientCallback extends UnicastRemoteObject implements ClientCallbac
 
     @Override
     public void disconnectClient() throws RemoteException {
+        view.endMatch("La partita è terminata perchè un player è uscito\n");
         UnicastRemoteObject.unexportObject(this, true);
+        System.exit(130);
     }
 
     @Override
